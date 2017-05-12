767f0cd85c2ca2504e826a39c61cdb7f4ed34f88
aisleypay <paisley.singh@gmail.com>
2017-05-12 10:28:46 -0400

    requirements

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -6,10 +6,30 @@ class AppearancesController < ApplicationController
 
   def create
     @appearance = Appearance.new(appearance_params)
+    @appearance.user_id = current_user.id
     if @appearance.save
       redirect_to @appearance.episode
     else
       redirect_to new_appearance_path
+    end
+  end
+
+  def show
+    @appearance = Appearance.find(params[:id])
+  end
+
+  def edit
+    @appearance = Appearance.find(params[:id])
+  end
+
+  def update
+    @appearance = Appearance.find(params[:id])
+
+    if @appearance.save
+      @appearance.update(appearance_params)
+      redirect_to appearance_path(@appearance)
+    else
+      redirect_to edit_appearance_path(@appearance)
     end
   end
 
